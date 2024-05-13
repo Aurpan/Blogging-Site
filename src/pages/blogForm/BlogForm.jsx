88ef336d093755c28e250796c79fe9cc7ff6015f
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2024-05-13 18:15:57 +0600

    Update BlogForm.jsx

--- src/pages/blogForm/BlogForm.jsx
+++ src/pages/blogForm/BlogForm.jsx
@@ -1,18 +1,31 @@
 import { InputText } from "primereact/inputtext";
 import { Editor } from "primereact/editor";
 import { Chips } from "primereact/chips";
+import { Button } from "primereact/button";
+import { useState } from "react";
 
 const BlogForm = () => {
+  const [titleText, setTitleText] = useState("");
+  const [contentText, setContentText] = useState("");
+  const [authorText, setAuthorText] = useState("");
+  const [tagsText, setTagsText] = useState("");
+
+  const blogFormSubmitHandler = (e) => {
+    // console.log(authorText);
+    // console.log(titleText);
+    console.log(contentText);
+  };
+
   return (
     <>
-      <div>
-        <h1>Create Blog</h1>
-      </div>
-
       <div className="card flex mb-6">
         <div className="flex flex-column gap-2 w-full">
           <label htmlFor="title">Title</label>
-          <InputText id="title" />
+          <InputText
+            id="title"
+            value={titleText}
+            onChange={(e) => setTitleText(e.target.value)}
+          />
         </div>
       </div>
 
@@ -20,7 +33,11 @@ const BlogForm = () => {
         <div className="flex flex-column gap-2 w-full">
           <label htmlFor="title">Blog Content</label>
           <div className="card">
-            <Editor style={{ height: "320px" }} />
+            <Editor
+              style={{ height: "320px" }}
+              value={contentText}
+              onTextChange={(e) => setContentText(e.htmlValue)}
+            />
           </div>
         </div>
       </div>
@@ -41,8 +58,28 @@ const BlogForm = () => {
       <div className="card flex mb-6">
         <div className="flex flex-column gap-2 md:w-full lg:w-6">
           <label htmlFor="title">Author Name</label>
-          <InputText id="title" />
+          <div className="card p-fluid">
+            <InputText
+              id="title"
+              value={authorText}
+              onChange={(e) => setAuthorText(e.target.value)}
+            />
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-wrap md:justify-content-center lg:justify-content-end">
+        <Button
+          label="Cancel"
+          className="flex justify-content-center border-round m-2"
+          outlined
+          severity="danger"
+        />
+        <Button
+          label="Submit"
+          className="flex justify-content-center border-round m-2"
+          onClick={blogFormSubmitHandler}
+        />
       </div>
     </>
   );
